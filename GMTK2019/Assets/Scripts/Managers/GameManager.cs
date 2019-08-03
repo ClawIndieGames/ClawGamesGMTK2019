f8d17f2a8ef7c33db8a2466e2e3c947c6bad2309
ClawIndieGames <50124235+ClawIndieGames@users.noreply.github.com>
2019-08-04 00:43:50 +0100

    Level 1 finished

--- GMTK2019/Assets/Scripts/Managers/GameManager.cs
+++ GMTK2019/Assets/Scripts/Managers/GameManager.cs
@@ -112,7 +112,11 @@ public class GameManager : MonoBehaviour
 
 
     private void Update()
-    {        
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ResetLevel();
+        }
     }
     
     #endregion
