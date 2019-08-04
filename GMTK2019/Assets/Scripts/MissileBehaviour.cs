058ece8b19ad3472a8b3cb1fe9aecc43d3439a6e
ClawIndieGames <50124235+ClawIndieGames@users.noreply.github.com>
2019-08-04 02:45:57 +0100

    Added homming missile

---
+++ GMTK2019/Assets/Scripts/MissileBehaviour.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileBehaviour : MonoBehaviour
+{
+    Transform target;
+    public Rigidbody2D rb;
+    public float speed;
+    public float rotationSpeed;
+    bool canChase;
+
+    void Start()
+    {
+        target = FindObjectOfType<PlayerController>().transform;        
+    }
+
+    
+    void FixedUpdate()
+    {
+        if (!canChase)
+            return;
+
+        var direction = (Vector2)target.position - rb.position;
+        direction.Normalize();
+                
+        float rotateAmmount = Vector3.Cross(direction, transform.up).z;
+
+        rb.angularVelocity = -rotateAmmount * rotationSpeed;
+        rb.velocity = transform.up * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            canChase = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Player")
+            || collision.transform.CompareTag("MousePlatform"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
